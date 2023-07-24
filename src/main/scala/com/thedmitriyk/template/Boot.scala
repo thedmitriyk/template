9892ded85ec0e66abd56457a8374f245fda4957f
Dmitriy Kostyuchenko <dmitriy@thedmitriyk.com>
2023-07-24 06:47:21 -0700

    Updated sbt version

--- src/main/scala/com/thedmitriyk/template/Boot.scala
+++ src/main/scala/com/thedmitriyk/template/Boot.scala
@@ -1,6 +1,6 @@
 package com.thedmitriyk.template
 
 object Boot extends App:
-    val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
+  val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
 
-    logger.info("Hello template!")
+  logger.info("Hello template!")
